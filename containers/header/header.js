56c115c153f0c9d533aae944e05b80fd1f554dca
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-06 20:38:43 -0300

    Create header container

---
+++ containers/header/header.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import IconButton from '../../components/icon-button'
+import Post from '../../icons/post'
+import Author from '../../icons/author'
+import Event from '../../icons/event'
+import styles from './header.module.scss'
+
+const routes = [
+  { title: 'Postagens', href: '/postagens', icon: Post },
+  { title: 'Autores', href: '/autores', icon: Author },
+  { title: 'Eventos', href: '/eventos', icon: Event }
+]
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <div className={styles.brand}>
+          <Link href='/'>
+            <img src='./images/white.webp' alt='Logo da Clínica Hospitalar Novo Nascer' loading='lazy'/>
+          </Link>
+        </div>
+        <nav className={styles.menu}>
+          <ul>
+            {routes.map(route => (
+              <li key={route.title}>
+                <IconButton {...route}/>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}

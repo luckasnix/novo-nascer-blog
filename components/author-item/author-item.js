67730b9dbaa8c954af19d341a42d01c76c09bd88
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-07 17:23:40 -0300

    Enhance styles of authors page

--- components/author-item/author-item.js
+++ components/author-item/author-item.js
@@ -4,19 +4,16 @@ import styles from './author-item.module.scss'
 
 export default function AuthorItem({ slug, profilePicture, name, occupation }) {
   return (
-    <li className={styles.authorItem}>
-      <div className={styles.picture}>
-        <img src={urlFor(profilePicture).width(128).url()} alt={`Foto do(a) ${name}`} loading='lazy'/>
-      </div>
-      <div className={styles.info}>
-        <h2>{name}</h2>
-        <span>{occupation}</span>
-        <Link href='/autores/[slug]' as={`/autores/${slug}`}>
-          <a>
-            <button>Ver Artigos</button>
-          </a>
-        </Link>
-      </div>
-    </li>
+    <Link href='/autores/[slug]' as={`/autores/${slug}`}>
+      <li className={styles.authorItem}>
+        <div className={styles.picture}>
+          <img src={urlFor(profilePicture).width(128).url()} alt={`Foto do(a) ${name}`} loading='lazy'/>
+        </div>
+        <div className={styles.info}>
+          <h2>{name}</h2>
+          <span>{occupation}</span>
+        </div>
+      </li>
+    </Link>
   )
 }

4a0c70aae65d3795943b5d88602de5c3ba80f7ad
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-06 22:02:51 -0300

    Create post list container

---
+++ containers/post-list/post-list.js
@@ -0,0 +1,16 @@
+import PostItem from '../../components/post-item'
+import styles from './post-list.module.scss'
+
+export default function PostList({ posts }) {
+  return (
+    <div className={styles.postList}>
+      <div className={styles.wrapper}>
+        <ul className={styles.list}>
+          {posts.map((post) => (
+            <PostItem key={post.slug} {...post}/>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}

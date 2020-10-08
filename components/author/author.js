6b5130df50cf3fcfbc2890f04565a1410f8f9f6b
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-08 17:46:33 -0300

    Style the content of post page

--- components/author/author.js
+++ components/author/author.js
@@ -1,16 +1,19 @@
+import Link from 'next/link'
 import { urlFor } from '../../utils/sanity'
 import styles from './author.module.scss'
 
-export default function Author({ profilePicture, name, occupation }) {
+export default function Author({ slug, profilePicture, name, occupation }) {
   return (
-    <div className={styles.author}>
-      <div className={styles.picture}>
-        <img src={urlFor(profilePicture).width(128).url()} alt={`Foto do(a) ${name}`} loading='lazy'/>
+    <Link href='/autor/[slug]' as={`/autor/${slug}`}>
+      <div className={styles.author}>
+        <div className={styles.picture}>
+          <img src={urlFor(profilePicture).width(128).url()} alt={`Foto do(a) ${name}`} loading='lazy'/>
+        </div>
+        <div className={styles.info}>
+          <h4>{name}</h4>
+          <span>{occupation}</span>
+        </div>
       </div>
-      <div className={styles.info}>
-        <h4>{name}</h4>
-        <span>{occupation}</span>
-      </div>
-    </div>
+    </Link>
   )
 }

985310284627c96ed82f45de1eb7f282f21ff787
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-13 10:12:14 -0300

    Convert layout container to TypeScript

--- containers/layout/layout.tsx
+++ containers/layout/layout.tsx
@@ -1,7 +1,12 @@
+import { ReactNode } from 'react'
 import Header from '../header'
 import Footer from '../footer'
 
-export default function Layout({ children }) {
+export interface LayoutProps {
+  children: ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header/>

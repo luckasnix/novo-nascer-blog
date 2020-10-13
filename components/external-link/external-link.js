01e34dbb5d68ab239041011c8a2ed479c926fa4e
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-12 21:39:02 -0300

    Convert external link component to TypeScript

--- components/external-link/external-link.js
+++
@@ -1,7 +0,0 @@
-export default function ExternalLink({ className, href, children }) {
-  return (
-    <a className={className} href={href} target='_blank' rel='noopener noreferrer'>
-      {children}
-    </a>
-  )
-}
---
+++ components/external-link/external-link.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+interface ExternalLinkProps {
+  className: string
+  href: string
+  children: ReactNode
+}
+
+export default function ExternalLink({ className, href, children }: ExternalLinkProps) {
+  return (
+    <a className={className} href={href} target='_blank' rel='noopener noreferrer'>
+      {children}
+    </a>
+  )
+}

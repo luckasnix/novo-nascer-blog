172647472b5743362ed67d674f8b8a3a567b1868
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-13 10:36:30 -0300

    Convert post list container to TypeScript

--- containers/post-list/post-list.tsx
+++ containers/post-list/post-list.tsx
@@ -1,12 +1,16 @@
-import PostItem from '../../components/post-item'
+import PostItem, { PostItemProps } from '../../components/post-item'
 import styles from './post-list.module.scss'
 
-export default function PostList({ posts }) {
+export interface PostListProps {
+  posts: PostItemProps[]
+}
+
+export default function PostList({ posts }: PostListProps) {
   return (
     <div className={styles.postList}>
       <div className={styles.wrapper}>
         <ul>
-          {posts.map((post) => (
+          {posts.map(post => (
             <PostItem key={post.slug} {...post}/>
           ))}
         </ul>
